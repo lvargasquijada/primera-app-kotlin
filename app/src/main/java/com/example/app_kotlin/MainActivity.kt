bfbab449325fc1f4a2db91901a7016e0c72b7a3b
Luis Felipe Vargas Quijada <luisfevq@gmail.com>
2026-02-18 21:39:33 -0300

    Finalizacion CounterApp

--- app/src/main/java/com/example/app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin/MainActivity.kt
@@ -6,21 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.app_kotlin.ui.theme.AppkotlinTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +44,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CounterApp() {
+
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
+            FloatingActionButton(onClick = { count++ }) {
                 Icon(Icons.Default.Add, contentDescription = "Increment")
             }
         }
@@ -56,9 +66,22 @@ fun CounterApp() {
                 text = "Presionaste el boton: "
             )
             Text(
-                text = "0",
+                text = "$count",
                 style = MaterialTheme.typography.displayLarge
             )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                OutlinedButton(onClick = { count-- }) {
+                    Text("-1")
+                }
+                OutlinedButton(onClick = { count = 0}) {
+                    Text(" 0 ")
+                }
+            }
         }
     }
 }
